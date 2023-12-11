f4e3b06b1fcec65d1def110ed569fc681a03e7f2
sholay1234 <sholay1104@gmail.com>
2023-12-11 11:04:15 +0530

    first commit

--- src/pages/basics.tsx
+++ src/pages/basics.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { BasicsView } from "../views";
 
 const Basics: NextPage = (props) => {
   return (
@@ -12,7 +11,6 @@ const Basics: NextPage = (props) => {
           content="🎉 *Introducing AO MINT: Your Gateway to Amigos Odyssey NFTs! 🚀*"
         />
       </Head>
-      <BasicsView />
     </div>
   );
 };
