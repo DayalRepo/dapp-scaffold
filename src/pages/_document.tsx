412be7e9dfe28d314c827f58fc36d3dfe85065b8
AO NEXUS LABS <121709547+SHOLAY1234@users.noreply.github.com>
2023-12-17 19:14:21 +0530

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document'
-import { link } from 'fs';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -11,19 +10,9 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <head>
-  <meta charset="utf-8"/>
-  <link rel="icon" href="/favicon.ico" />
-  <meta name="viewport" content="width=device-width,initial-scale=1"/>
-  <meta name="theme-color" content="#000000"/>
-  <meta name="description" content="Web site created using create-react-app"/>
-  <link rel="apple-touch-icon" href="/logo.jpg"/>
-  <link rel="manifest" href="/manifest.json"/>
-  <title>AMIGOS ODYSSEY</title>
-  <link href="/static/css/main.c5685a7e.css" rel="stylesheet">
-  <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
-  <script defer="defer" src="/static/js/main.0108abfd.js"></script>
-</head>
+        <Head>
+        <link rel="shortcut icon" href="/favicon.ico?v=2" />
+        </Head>
         <body>
           <Main />
           <NextScript />
