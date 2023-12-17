441775c67480f356196eb25fc9c759b097ce8982
AO NEXUS LABS <121709547+SHOLAY1234@users.noreply.github.com>
2023-12-17 19:07:42 +0530

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,9 +10,20 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-        <link rel="shortcut icon" href="/favicon.ico?v=2" />
-        </Head>
+        <head>
+  <meta charset="utf-8"/>
+  <link rel="icon" href="/favicon.ico" />
+  <meta name="viewport" content="width=device-width,initial-scale=1"/>
+  <meta name="theme-color" content="#000000"/>
+  <meta name="description" content="Web site created using create-react-app"/>
+  <link rel="apple-touch-icon" href="/logo.jpg"/>
+  <link rel="manifest" href="/manifest.json"/>
+  <title>AMIGOS ODYSSEY</title>
+  <link href="/static/css/main.c5685a7e.css" rel="stylesheet">
+  <!-- Add the following line to include the favicon for Google search results -->
+  <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+  <script defer="defer" src="/static/js/main.0108abfd.js"></script>
+</head>
         <body>
           <Main />
           <NextScript />
